ac27a99e12faaf93ef929277b62a4968f24f2b88
b4stet <ptitgnu404@gmail.com>
2020-06-02 20:27:10 +0200

    prevented unittest discover to run on runner (bis).

--- runner_tests.py
+++ runner_tests.py
@@ -1,17 +1,20 @@
 import unittest
 
 from src.converter import test_time
-from src.validator import test_input, test_user
+from src.validator import test_input
+from src.validator import test_user as test_user_validator
 from src.crypto import test_crypter, test_hasher
+from src.table import test_user as test_user_table
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 suite.addTests(loader.loadTestsFromModule(test_time))
 suite.addTests(loader.loadTestsFromModule(test_input))
-suite.addTests(loader.loadTestsFromModule(test_user))
+suite.addTests(loader.loadTestsFromModule(test_user_validator))
 suite.addTests(loader.loadTestsFromModule(test_crypter))
 suite.addTests(loader.loadTestsFromModule(test_hasher))
+suite.addTests(loader.loadTestsFromModule(test_user_table))
 
 runner = unittest.TextTestRunner(verbosity=3)
 runner.run(suite)
